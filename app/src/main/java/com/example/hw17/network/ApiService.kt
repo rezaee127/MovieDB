c3605a1612c3ee4d3036f8c03efd869886f9d21c
razi <rezaie127@yahoo.com>
2022-05-26 00:54:41 +0430

    add Interceptor

--- app/src/main/java/com/example/hw17/network/ApiService.kt
+++ app/src/main/java/com/example/hw17/network/ApiService.kt
@@ -7,6 +7,8 @@ import com.example.hw17.models.Popular
 import com.example.hw17.models.Trailer
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
@@ -23,9 +25,16 @@ private val moshi = Moshi.Builder()
     .build()
 
 
+val logger = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC }
+
+val client = OkHttpClient.Builder()
+    .addInterceptor(logger)
+    .build()
+
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory .create(moshi))
     .baseUrl(BASE_URL)
+    .client(client)
     .build()
 
 
@@ -50,6 +59,7 @@ interface ApiService {
         @Path("movie_id")movieId:Int,
         @Query("api_key")key:String= API_KEY
     ): Detail
+
 
     @GET("movie/{movie_id}/videos")
     suspend fun getVideo(
