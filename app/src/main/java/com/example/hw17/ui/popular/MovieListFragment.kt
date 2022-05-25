ff1e40c97b7e35769d811a1306677e0e47cc6526
razi <rezaie127@yahoo.com>
2022-05-26 01:01:42 +0430

    add show video to MovieListFragment

--- app/src/main/java/com/example/hw17/ui/popular/MovieListFragment.kt
+++ app/src/main/java/com/example/hw17/ui/popular/MovieListFragment.kt
@@ -2,6 +2,7 @@ package com.example.hw17.ui.popular
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -31,14 +32,28 @@ class MovieListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initView()
+        initAdapter()
     }
 
-    private fun initView() {
-       val adapter= MovieAdapter(){
-           val bundle=bundleOf("id" to it)
-           findNavController().navigate(R.id.action_movieListFragment_to_detailFragment,bundle)
-       }
+    private fun initAdapter() {
+
+       val adapter= MovieAdapter({id->
+           vModel.getMovieDetail(id).observe(viewLifecycleOwner) {
+              // if(it!=null){
+                   val bundle=bundleOf("id" to id)
+                   findNavController().navigate(R.id.action_movieListFragment_to_detailFragment,bundle)
+//               }else
+//                   Toast.makeText(requireContext(),"There are no details for this movie",Toast.LENGTH_SHORT).show()
+           }
+       },{ movieId->
+           vModel.getVideo(movieId).observe(requireActivity()) {
+               var videoKey = it[0].key
+               val videoUrl = "https://www.youtube.com/watch?v=${videoKey}"
+               val bundle = bundleOf("url" to videoUrl)
+               findNavController().navigate(R.id.action_movieListFragment_to_showVideoFragment, bundle)
+           }
+
+       })
        binding.rvMovie.adapter=adapter
        vModel.listMovie.observe(viewLifecycleOwner){
            adapter.submitList(it)
@@ -54,7 +69,6 @@ class MovieListFragment : Fragment() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle item selection
         return when (item.itemId) {
             R.id.coming_soon_menu_item  -> {
                 findNavController().navigate(R.id.action_movieListFragment_to_comingSoonFragment)
--- app/src/main/java/com/example/hw17/ui/popular/MovieListViewModel.kt
+++ app/src/main/java/com/example/hw17/ui/popular/MovieListViewModel.kt
@@ -2,15 +2,21 @@ package com.example.hw17.ui.popular
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.hw17.data.Repository
+import com.example.hw17.models.Detail
 import com.example.hw17.models.Movie
+import com.example.hw17.models.Video
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 
 class MovieListViewModel(app:Application):AndroidViewModel(app) {
     var value=""
     var listMovie=MutableLiveData<List<Movie>>()
+    var movie=MutableLiveData<Detail>()
+
 
     init {
         getPopularList()
@@ -27,6 +33,26 @@ class MovieListViewModel(app:Application):AndroidViewModel(app) {
             }
 
         }
+    }
+
+    fun getMovieDetail(movieId:Int): LiveData<Detail> {
+        viewModelScope.async {
+            try{
+                movie.value=Repository.getMovieDetail(movieId)
+            }
+            catch (e: java.lang.Exception){
+
+            }
+        }
+        return movie
+    }
+
+    fun getVideo(id:Int):LiveData<List<Video>>{
+        var videoList=MutableLiveData<List<Video>>()
+        viewModelScope.launch {
+            videoList.value=Repository.getVideo(id).videos
+        }
+        return videoList
     }
 
 }
